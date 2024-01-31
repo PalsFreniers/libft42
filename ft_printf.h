f7821ff4260f8abf7138b52037c657a60e0b5b5c
Tidian Delage <tdelage@3G5.42angouleme.fr>
2024-01-31 08:15:40 +0100

    Initial Commit

---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdelage <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 21:41:05 by tdelage           #+#    #+#             */
+/*   Updated: 2024/01/25 22:58:50 by tdelage          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h>
+# define STDOUT 1
+
+int	ft_vfprintf(int fd, const char *fmt, va_list lst);
+int	ft_fprintf(int fd, const char *fmt, ...);
+int	ft_vprintf(const char *fmt, va_list lst);
+int	ft_printf(const char *fmt, ...);
+int	print_printf_specials(int fd, const char *fmt, va_list lst, size_t *index);
+
+#endif // FT_PRINTF_H
